00d47debf4fbd212511bf4957d8d3d595095c996
Stanley Louis <stanlouis@msn.com>
2019-01-23 07:50:19 -0500

    Add no entry in db case to PlayerList

--- imports/ui/PlayerList.jsx
+++ imports/ui/PlayerList.jsx
@@ -4,17 +4,21 @@ import Player from './Player';
 const PlayerList = props => {
   const { playersList, handleDelete, handleIncrement, handleDecrement } = props;
 
-  return playersList.map(player => {
-    return (
-      <Player
-        key={player._id}
-        playerData={player}
-        handleDelete={handleDelete}
-        handleIncrement={handleIncrement}
-        handleDecrement={handleDecrement}
-      />
-    );
-  });
+  return playersList.length === 0 ? (
+    <p>Add first player to start.</p>
+  ) : (
+    playersList.map(player => {
+      return (
+        <Player
+          key={player._id}
+          playerData={player}
+          handleDelete={handleDelete}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+        />
+      );
+    })
+  );
 };
 
 export default PlayerList;
